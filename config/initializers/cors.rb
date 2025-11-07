afbd6d30e577d6e7612bfebfaebc0ae61cb6f1d2
Ivan Vallejos <ivanvallejos06@gmail.com>
2025-11-07 12:28:05 -0700

    configuracion de CORS realizada para integracion con Frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,20 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    if Rails.env.development?  
+      allowed_origins = ENV['ALLOWED_ORIGINS']&.split(',')
+      origins(*allowed_origins)
+    else
+      # Produccion: solo el dominio frontend
+      origins ENV.fetch['FRONTEND_URL']
+    end
+
+    resource "/api/*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: false,
+      max_age: 600
+  end
+end
